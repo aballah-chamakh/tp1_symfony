9d40feadad9104b7ec46aeee77c887768f9b9219
abdallahchamakh <chamakhabdallah8@gmail.com>
2021-05-27 09:59:05 +0100

    first commit

---
+++ src/Entity/Fournisseur.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\FournisseurRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=FournisseurRepository::class)
+ */
+class Fournisseur
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+}
